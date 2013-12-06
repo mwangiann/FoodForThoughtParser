227ea234f4df659a7a182253f4a83e9710556856
Ann Mwangi <ann@Anns-MacBook-Pro.local>
2013-12-06 16:22:39 +0300

    removed additional files files root

--- item.py
+++
@@ -1,6 +0,0 @@
-class Item(object):
-    def __init__(self, item_dict):
-        self.id = item_dict['id']
-        self.name = item_dict['name']
-        self.price = item_dict['price']
-        self.available = item_dict['available']
