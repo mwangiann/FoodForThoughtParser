2f26ea16bca0293b76283679ae396d2529e5a9be
Jafari <jafaris@thoughtworks.com>
2013-12-06 20:17:56 +0300

    added user_who_ordered_most_of() method in parser, but it is not yet refactored

--- src/parser.py
+++ src/parser.py
@@ -33,3 +33,31 @@ class Parser:
 
 	    return most_expensive_order
 
+	def user_who_ordered_most_of(self, item_id): # n.b: yet to be refactored
+		order_item_list = []
+		for order in order_list:
+			if len(filter (lambda x : x.item_id == item_id, order.items)) > 0:
+				order_item_list.append(order)
+
+		user_and_no_ordered = {}
+		for order in order_item_list:
+			if user_and_no_ordered.has_key(order.user_id):
+				user_and_no_ordered[order.user_id] = user_and_no_ordered[order.user_id] + 1
+			else:
+				user_and_no_ordered.append(order.user_id, 1)
+
+		max_no_ordered =  max(user_and_no_ordered.values())
+
+		users_with_max_orders = {}
+
+		for key, value in user_and_no_ordered.items():
+			if value == max_no_ordered:
+				users_with_max_orders = {key, value}
+
+		return users_with_max_orders
+
+
+
+
+
+			
